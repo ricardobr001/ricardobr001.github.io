19d7bf19f133153de380737d60c83cdc3fcf67dc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-05 18:18:51 +0000

    Deploy ricardobr001/ricardobr001.github.io to github.com/ricardobr001/ricardobr001.github.io.git:master

---
+++ precache-manifest.4e15622ecf004cf51ce7085c7ff78ee1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64fff98d09a340cf621dfb632684e65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b37f0af693f251f5c042",
+    "url": "/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "b37f0af693f251f5c042",
+    "url": "/static/js/2.51ae501b.chunk.js"
+  },
+  {
+    "revision": "0be941549e3ca7b35a3f",
+    "url": "/static/js/main.7f909e4b.chunk.js"
+  },
+  {
+    "revision": "e92311ac86b52f5370c6",
+    "url": "/static/js/runtime-main.958cec56.js"
+  },
+  {
+    "revision": "cf7beb3543ffc09faae9cc0a6fe24307",
+    "url": "/static/media/cv-en.cf7beb35.pdf"
+  },
+  {
+    "revision": "9a6eb1d8a82bd245f9affc4c43f12923",
+    "url": "/static/media/cv-pt.9a6eb1d8.pdf"
+  },
+  {
+    "revision": "d4b60d3b7c11c97c6448351412947c85",
+    "url": "/static/media/me.d4b60d3b.jpeg"
+  },
+  {
+    "revision": "2fa9bea4d33fc903c7abe3bd0aec32ae",
+    "url": "/static/media/rockbandnotfound.2fa9bea4.png"
+  },
+  {
+    "revision": "1e3fc0bb6223b5540714362976af7dd1",
+    "url": "/static/media/wolf.1e3fc0bb.jpg"
+  }
+]);
